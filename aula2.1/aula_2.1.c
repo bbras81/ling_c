5c4b5c5ea78719e99159d6db6c4a3644e54ed15e
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-12-13 18:26:39 +0000

    Auto-commit: 2024-12-13 18:26:39

---
+++ aula2.1/aula_2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("Ola tudo bem? Vamos programar!\n");
+
+    printf("Valor inteiro: %d.\n", 10);
+    printf("Valor float: %f.\n", 3.14159265);
+    printf("Valor char: %c.\n", 'A');
+    printf("Valor string: %s.\n", "Testando");
+    printf("\a");
+
+    return 0;
+}   
