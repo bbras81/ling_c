41f1dd2d0485ef2355c91c83e1eca5e123e3a9e4
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-12-24 18:10:52 +0000

    Ling_c directories

---
+++ aula4.1/aula_4.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+    float nota;
+
+    printf("Digite uma nota:");
+    scanf("%f", &nota);
+
+
+    if(nota >= 7){
+        printf("Aluno aprovado.");
+    }
+}
